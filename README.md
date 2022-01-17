209cf5e583e30116c88a6ce6b05c60d3ea12c921
Catherine Viloria <gusviloca@GU.GU.SE@mltgpu.flov.gu.se>
2022-01-17 02:08:19 +0100

    eval edits

--- README.md
+++ README.md
@@ -7,4 +7,5 @@
 
 #### References 
 - on [Multi-label Model Evaluation](https://www.kaggle.com/kmkarakaya/multi-label-model-evaluation)
-- on [Multi-Class Metrics](https://towardsdatascience.com/multi-class-metrics-made-simple-part-i-precision-and-recall-9250280bddc2)
+- on [Multi-Class Metrics](https://towardsdatascience.com/multi-class-metrics-made-simple-part-i-precision-and-recall-9250280bddc2), [PT.II](https://towardsdatascience.com/multi-class-metrics-made-simple-part-ii-the-f1-score-ebe8b2c2ca1)
+- [sklearn Metrics and Scoring](https://scikit-learn.org/stable/modules/model_evaluation.html#classification-metrics)
--- eval.py
+++ eval.py
@@ -1,10 +1,21 @@
 # Evaluate accuracy, precision, recall, and F1 of smokerstatus.py
+
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-# from sklearn.metrics import classification_report TRY THIS LATER
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+from sklearn.metrics import balanced_accuracy_score 
+
+import pandas as pd
 
 
 # import functions
 # from smokerstatus import get_gold
+
+# export statistics as text file
+    # if possible, take all the incorrect instances and include in text file
+
+    # txt file name as csvname-eval.txt
 
 def evaluate_fst(gold_labels, preds, datacsv):
     """
@@ -34,24 +45,16 @@ def evaluate_fst(gold_labels, preds, datacsv):
 
     # --- begin calculations ---
 
-    # accuracy (normalize = True --> fraction of correctly classified samples, 
-    #   vs False -->  number of correctly classified samples)
-    accuracy = accuracy_score(gold_statuses, pred_statuses)
-
-    # balanced accuracy score
-        # average of recall obtained on each class
-        # ** is this appropriate? think about this 
-    # balanced_accuracy = balanced_accuracy_score(gold_statuses, pred_statuses)
-
     # confusion matrix
     cm = confusion_matrix(gold_statuses, pred_statuses)
 
     disp = ConfusionMatrixDisplay(confusion_matrix = cm, 
                     display_labels = ['Former', 'Non', 'Smoker', 'Unknown'])
-    disp.plot() # necessary?
+    disp.plot(cmap="YlOrRd") 
 
+    # save confusion matrix
     data_name = datacsv[:-4]
-    cm_path = data + '-cm.png'
+    cm_path = data_name + '-cm.png'
     plt.savefig(cm_path)
 
     # classification report - precision, recall, F1, support (# of samples for each class)
@@ -66,25 +69,69 @@ def evaluate_fst(gold_labels, preds, datacsv):
                                      target_names = label_names,
                                      output_dict = True)
     classreport_df = pd.DataFrame.from_dict(class_report_dict).transpose()
-    cr_path = data + '-classreport.csv'
+    cr_path = data_name + '-classreport.csv'
     classreport_df.to_csv(cr_path, index=False)
 
+    print('Exported classification report to ', cr_path)
 
+    # accuracy (normalize = True --> fraction of correctly classified samples, 
+        # vs False -->  number of correctly classified samples)
+    # accuracy = accuracy_score(gold_statuses, pred_statuses)
+
+    # balanced accuracy score
+        # average of recall obtained on each class
+        # ** is this appropriate? think about this 
+    # balanced_accuracy = balanced_accuracy_score(gold_statuses, pred_statuses)
+      
     # precision
         # true positives / (true positives + false positives)
         # ability not to label as positive a sample that is negative
+        # micro: globally --> total true positives, false negatives, and false positives
+        # macro: each label --> unweighted mean (doesn't consider label imbalance)
+        # weighted: metrics for each label, average weighted by support 
+        #       --> F-score not btwn precision&recall
     # precision = precision_score(gold_statuses, pred_statuses, average=None)
 
     # recall
 
     # F1
 
-# print on terminal statistics
-
-# export statistics as text file
-    # if possible, take all the incorrect instances and include in text file
-
-    # txt file name as csvname-eval.txt
-
 # print all wrong instances in .csv
     # gold, pred, identifier
+
+def get_wrong_labels(gold_labels, preds, datacsv):
+    """
+    Identify instances that are labelled wrong and what they were labelled. 
+
+    Args:
+        - gold_labels - (type dict) {patient-identifier: status, patient-identifier: status...}
+        - preds - (type dict) {patient-identifier: predicted-status, ...}
+    
+    Returns:
+        - exports .csv file (gold, predicted, identifier/row_id)
+    """
+
+    incorrectly_labelled = []
+
+    for identifier in gold_labels.keys():
+
+        truth = gold_labels[identifier]
+        pred = preds[identifier]
+
+        # if wrong prediction
+        if truth != pred:
+            row = (truth, pred, identifier)
+            incorrectly_labelled.append(row)
+    
+    wrong_labels_df = pd.Dataframe(incorrectly_labelled, columns = ['gold', 'predicted', 'identifier'])
+
+    # export to csv file
+    data_name = datacsv[:-4]
+    wrong_labels_path = data_name + '-wronglabels.csv'
+    wrong_labels_df.to_csv(wrong_labels_path, index=False)
+
+    print(*incorrectly_labelled, sep="\n")
+
+
+
+
